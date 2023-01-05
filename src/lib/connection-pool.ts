54a89dbfeb43058dccf86b3a4dcb3ce435199d00
Sayem Shafayet <ishafayet@gmail.com>
2023-01-05 16:51:37 +0600

    Add implied round-robin connection selection

--- src/lib/connection-pool.ts
+++ src/lib/connection-pool.ts
@@ -5,32 +5,66 @@ import { CodedError } from "../utility/coded-error.js";
 
 export class ConnectionPool {
   private uidSeed = 0;
-  private connectionMap!: Record<string, HowsWebSocket>;
+
+  private availableConnectionMap!: Map<string, HowsWebSocket>;
+  private occupiedConnectionMap!: Map<string, HowsWebSocket>;
 
   constructor() {
-    this.connectionMap = {};
+    this.availableConnectionMap = new Map<string, HowsWebSocket>();
+    this.occupiedConnectionMap = new Map<string, HowsWebSocket>();
   }
 
   private getNewUid(): string {
     return `ws${this.uidSeed++}`;
   }
 
-  // TODO: implement roundrobin
-  getAConnection(): WebSocket {
-    let keys = Object.keys(this.connectionMap);
-
-    if (keys.length === 0) {
-      throw new CodedError(ErrorCode.NO_WORKER_AVAILABLE);
-    }
-
-    let randomUidIndex = Math.floor(Math.random() * keys.length - 1);
-    let randomUid = `ws${keys[randomUidIndex]}`;
-    return this.connectionMap[randomUid];
-  }
-
   addToPool(ws: WebSocket) {
     let uid = this.getNewUid();
-    this.connectionMap[uid] = ws as HowsWebSocket;
     (ws as HowsWebSocket).uid = uid;
+
+    ws.once("close", () => {
+      logger.log(`CPOOL: ${uid}: Connection closed.`);
+      this.availableConnectionMap.delete(uid);
+      this.occupiedConnectionMap.delete(uid);
+      this.reportConnectionStatus();
+    });
+
+    this.availableConnectionMap.set(uid, ws as HowsWebSocket);
+    this.reportConnectionStatus();
+  }
+
+  getAnAvailableConnection(): HowsWebSocket {
+    logger.log(`CPOOL: An available connection is requested.`);
+
+    if (this.availableConnectionMap.size === 0) {
+      throw new CodedError(
+        ErrorCode.NO_WORKER_AVAILABLE,
+        `${ErrorCode.NO_WORKER_AVAILABLE}: No worker is available to handle the request`
+      );
+    }
+
+    let [uid, ws] = this.availableConnectionMap.entries().next().value;
+    this.occupiedConnectionMap.set(uid, ws);
+
+    logger.log(`CPOOL: ${uid}: Leasing connection.`);
+    this.reportConnectionStatus();
+
+    return ws;
+  }
+
+  returnSocketBackToPoolIfOpen(ws: HowsWebSocket) {
+    if (!ws) return;
+    logger.log(`CPOOL: ${ws.uid}: connection is being returned to the pool.`);
+    if (!ws.OPEN) return;
+    if (!this.occupiedConnectionMap.has(ws.uid)) return;
+    this.occupiedConnectionMap.delete(ws.uid);
+    this.availableConnectionMap.set(ws.uid, ws);
+    logger.log(`CPOOL: ${ws.uid}: connection has been returned.`);
+    this.reportConnectionStatus();
+  }
+
+  reportConnectionStatus() {
+    let message = `Available connections: ${this.availableConnectionMap.size}, occupied connections: ${this.occupiedConnectionMap.size}`;
+    logger.debug(`CPOOL: ${message}`);
   }
 }
