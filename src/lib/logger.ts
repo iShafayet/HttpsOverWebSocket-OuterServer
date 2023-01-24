5cdc1907d3c5cd7a42c181deefd177b620115b05
Sayem Shafayet <ishafayet@gmail.com>
2023-01-24 22:02:36 +0600

    Revamp logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,11 @@
 const STYLE = {
-  FgYellow: "\x1b[33m"
+  FgYellow: "\x1b[33m",
+  FgRed: "\x1b[31m",
+  FgGray: "\x1b[90m",
+  Reset: "\x1b[0m",
+  BgBlue: "\x1b[44m",
+  BgMagenta: "\x1b[45m",
+  BgRed: "\x1b[41m",
 };
 
 class Logger {
@@ -9,7 +15,7 @@ class Logger {
     important: true,
     warning: true,
     error: true,
-    urgent: true,
+    critical: true,
   };
 
   constructor({
@@ -19,7 +25,7 @@ class Logger {
       warning = true,
       error = true,
       important = true,
-      urgent = true,
+      critical = true,
     },
   }: any = {}) {
     this.switches = {
@@ -28,7 +34,7 @@ class Logger {
       important,
       warning,
       error,
-      urgent,
+      critical,
     };
   }
 
@@ -38,45 +44,105 @@ class Logger {
 
   debug(...args: any) {
     if (!this.switches.debug) return;
-    console.log.apply(console, [STYLE.FgYellow + "DEBUG\t", ...args]);
+    console.log.apply(console, [this.makePrefix("DEBUG"), ...args]);
   }
 
   log(...args: any) {
     if (!this.switches.log) return;
-    console.log.apply(console, ["LOG\t", ...args]);
+    console.log.apply(console, [this.makePrefix("LOG"), ...args]);
   }
 
-  urgent(...args: any) {
-    if (!this.switches.important) return;
+  critical(...args: any) {
+    if (!this.switches.critical) return;
     args.forEach((arg: any, index: number) => {
       args[index] = JSON.stringify(arg, null, 2);
     });
-    console.log.apply(console, ["URGENT\t", ...args]);
+    console.log.apply(console, [this.makePrefix("CRITICAL"), ...args]);
   }
 
   important(...args: any) {
     if (!this.switches.important) return;
-    console.log.apply(console, ["IMPORTANT\t", ...args]);
+    console.log.apply(console, [this.makePrefix("IMPORTANT"), ...args]);
   }
 
-  warn(errorObject: Error, optionalContext = null) {
+  warn(errorObject: Error, optionalContext: any = null) {
+    try {
+      let errorString = this.stringifyError(errorObject);
+
+      let args = [this.makePrefix("WARN"), errorString];
+      if (optionalContext) {
+        args.push("| Context:");
+        args.push(optionalContext);
+      }
+      console.log.apply(console, args);
+    } catch (ex) {
+      let args = [this.makePrefix("WARN"), errorObject];
+      if (optionalContext) {
+        args.push("| Context:");
+        args.push(optionalContext);
+      }
+      console.log.apply(console, args);
+    }
+
     console.warn(errorObject);
-
-    let errorString = JSON.stringify(
-      errorObject,
-      Object.getOwnPropertyNames(errorObject)
-    );
-    console.log.apply(console, ["IMPORTANT\t", errorString, optionalContext]);
   }
 
-  error(errorObject: Error, optionalContext = null) {
-    console.error(errorObject);
+  error(errorObject: Error, optionalContext: any = null) {
+    try {
+      let errorString = this.stringifyError(errorObject);
 
-    // let errorString = JSON.stringify(
-    //   errorObject,
-    //   Object.getOwnPropertyNames(errorObject)
-    // );
-    // console.log.apply(console, ["IMPORTANT\t", errorString, optionalContext]);
+      let args = [this.makePrefix("ERROR"), errorString];
+      if (optionalContext) {
+        args.push("| Context:");
+        args.push(optionalContext);
+      }
+      console.log.apply(console, args);
+    } catch (ex) {
+      let args = [this.makePrefix("ERROR"), errorObject];
+      if (optionalContext) {
+        args.push("| Context:");
+        args.push(optionalContext);
+      }
+      console.log.apply(console, args);
+    }
+
+    console.error(errorObject);
+  }
+
+  private makePrefix(logType: string) {
+    let color = "";
+    if (logType === "DEBUG") {
+      color = STYLE.FgYellow;
+    } else if (logType === "LOG") {
+      color = STYLE.FgGray;
+    } else if (logType === "WARN") {
+      color = STYLE.FgRed;
+    } else if (logType === "IMPORTANT") {
+      color = STYLE.BgBlue;
+    } else if (logType === "CRITICAL") {
+      color = STYLE.BgMagenta;
+    } else if (logType === "ERROR") {
+      color = STYLE.BgRed;
+    }
+    let datetime = new Date().toISOString();
+    return `${color}[${process.pid}, ${logType}, ${datetime}]${STYLE.Reset}`;
+  }
+
+  private stringifyError(errorObject: Error) {
+    let errorString = "";
+    if ((errorObject as any).name) {
+      errorString += (errorObject as any).name + ", ";
+    }
+
+    if ((errorObject as any).code) {
+      errorString += (errorObject as any).code + ", ";
+    }
+
+    if (errorObject.message) {
+      errorString += errorObject.message;
+    }
+
+    return errorString;
   }
 }
 
--- src/start.ts
+++ src/start.ts
@@ -4,9 +4,23 @@ import {
   lookupAndLoadConfigAsync,
 } from "./utility/startup-utils.js";
 import { HttpsOverWebSockerOuterServer } from "./index.js";
+import { Logger } from "./lib/logger.js";
+
+// We initiate logger and inject it into global so that it is usable everywhere.
+global.logger = new Logger({
+  switches: {
+    debug: true,
+    log: true,
+    important: true,
+    warning: true,
+    error: true,
+    critical: true,
+  },
+});
+logger.init();
 
 let commandLineParams = extractProcessParams();
-console.log("STARTUP Application parameters: ", commandLineParams);
+logger.log("STARTUP Application parameters: ", commandLineParams);
 
 let config: Config = lookupAndLoadConfigAsync(commandLineParams);
 new HttpsOverWebSockerOuterServer().start(config);
